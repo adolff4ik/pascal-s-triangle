0bb5ccb90e00ad70b6b00f5f8b12639c7cba184e
Adolff4ik <109664845+adolff4ik@users.noreply.github.com>
2022-12-18 01:16:03 +0200

    pascal's triangle

---
+++ trikytnik.py
@@ -0,0 +1,19 @@
+print(' 0\n010')
+list = [0,1,0]
+
+for i in range(100):
+    newlist = []
+    for q in range(len(list)):
+        if q < 100 and int(len(list) - 1) != q:
+            newnum = int(list[q] + list[q+1])
+            newlist.append(newnum)
+        else:
+            break
+    list.clear()
+
+    list.append(0)
+    for l in newlist:
+        list.append(l)
+    list.append(0)
+    
+    print(list)
